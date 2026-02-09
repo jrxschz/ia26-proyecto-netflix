c7a60237c99f53a89e5f54596c4c3d3fd4a7e9c8
Sanaz <49277985+sanazamann@users.noreply.github.com>
2026-02-09 09:24:26 +0100

    repositorio

--- src/download.py
+++ src/download.py
@@ -1,9 +1,9 @@
-
+import os
+import json
 import requests
 
 from config import ACCESS_TOKEN
 
-url_prueba = "https://api.themoviedb.org/3/authentication"
 url_popular_movies = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
 
 headers = {
@@ -12,5 +12,14 @@ headers = {
 }
 
 response = requests.get(url_popular_movies, headers=headers)
+movies = response.json()["results"]
 
-print(response.json()["results"])
+os.makedirs("data/raw", exist_ok=True)
+
+#Guardar el archivo
+file_path = "data/raw/popular_movies.json"
+
+with open(file_path, "w", encoding="utf-8") as f:
+    json.dump(movies, f, indent=4, ensure_ascii=False)
+
+print(f"Se guardaron {len(movies)} películas en {file_path}")
